746e3e2b055f4a10223f7a6d480b5b53dcd27dcf
Dilm SEO <145151224+dilm-seo@users.noreply.github.com>
2025-01-16 20:08:09 +0100

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,24 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
   server: {
-    historyApiFallback: true
+    port: 5173,
+    strictPort: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5173',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  },
+  preview: {
+    port: 5173,
+    strictPort: true,
   }
 });
